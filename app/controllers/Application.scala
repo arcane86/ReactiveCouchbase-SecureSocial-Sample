9a25c3f1678342861dbce13695850072dd313927
Matthieu Clochard <clochard.matthieu@gmail.com>
2014-01-11 10:59:45 +0100

    Add routes for SecureSocial and secured test pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,24 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+object Application extends Controller with securesocial.core.SecureSocial {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+    def index = Action {
+        Redirect("/userAware")
+    }
 
+    def secured = SecuredAction {
+        implicit request =>
+            Ok(views.html.index(request.user.fullName))
+    }
+
+    def userAware = UserAwareAction {
+        implicit request =>
+            val userName = request.user match {
+                case Some(user) => user.fullName
+                case _ => "guest"
+            }
+            Ok(views.html.index(userName))
+    }
 }
